beb9dab6ee163d0b2e8d0b6514f425f74f482f6e
thisunravisara <thisunravisara@gmail.com>
2019-09-27 14:56:31 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ const { convert } = require('./convert-svg-to-png');
 
 exports.handler = async event => {
     const body = JSON.parse( event.body );
-    const png = await convert(body.svg, { width: body.width, height: body.height });
-    return {
-      statusCode: 200,
-      body: png,
-      // FIXME: Should be able to directly set the buffer instead of base64 encodeed string
-      //        find why it doesn't work setting buffer to the body in serverless
-      isBase64Encoded: true,
-  
-      headers: {
-        "content-type": "image/png"
-      }
-    };    
+    if ( !body.svg ) {
+        return {
+            statusCode: 500,
+            body: 'svg is missiing',
+        }
+    }
+
+    try {
+        const png = await convert(body.svg, { width: body.width, height: body.height });
+        return {
+          statusCode: 200,
+          body: png,
+          // FIXME: Should be able to directly set the buffer instead of base64 encodeed string
+          //        find why it doesn't work setting buffer to the body in serverless
+          isBase64Encoded: true,
+      
+          headers: {
+            "content-type": "image/png"
+          }
+        };       
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Failed to generate svg',
+        }
+    }
 }
